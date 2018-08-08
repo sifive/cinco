eacd1e7cf3ec0a93e69deb7238d1bea5293dbcbd
Michael Zaidman <michael.zaidman@wdc.com>
2018-08-08 14:01:58 +0300

    libraries: add examples of RGBL usage
    The following examples are added:
    
    - blink RGB LED via RGBL APIs
    - fade RGB LED via RGBL APIs
    
    Signed-off-by: Michael Zaidman <michael.zaidman@wdc.com>

---
+++ hardware/freedom_e/libraries/RGBL/examples/RgbLedBlink/RgbLedBlink.ino
@@ -0,0 +1,27 @@
+/*
+  Example of the HiFive1 builtin RGB LED blinking with RGBL library.
+
+  On HiFive1 the RGB LED is connected to the following GPIOs:
+    Red   - GPIO6
+    Green - GPIO3
+    Blue  - GPIO5
+
+  This example code is in the public domain.
+
+  By Michael Zaidman <michael.zaidman@wdc.com>, 2018
+*/
+
+#include <RGBL.h>
+
+RGBL led;
+
+void setup() {
+  led.offAll();
+}
+
+void loop() {
+  led.blink(RED, 1000);
+  led.blink(GREEN, 1000);
+  led.blink(BLUE, 1000);
+}
+
