8ad8f4d6d902be6b2643d05c995268e1e6c74879
Michael Zaidman <michael.zaidman@wdc.com>
2018-09-07 15:37:14 +0300

    wiring: code style cleanup
    Signed-off-by: Michael Zaidman <michael.zaidman@wdc.com>

--- hardware/freedom_e/cores/arduino/wiring.h
+++ hardware/freedom_e/cores/arduino/wiring.h
@@ -22,11 +22,8 @@
 
 __BEGIN_DECLS
 
-/**
- *
- */
-extern void initVariant( void ) ;
-extern void init( void ) ;
+extern void initVariant(void);
+extern void init(void);
 
 struct variant_pin_map_s {
   uint8_t io_port;
@@ -45,7 +42,7 @@ typedef struct {
   uint32_t n;
   uint32_t mult;
   uint32_t shift;
-} int_inverse ;
+} int_inverse;
 
 extern int_inverse f_cpu_1000_inv;
 extern int_inverse f_cpu_1000000_inv;
@@ -55,30 +52,30 @@ void calc_inv(uint32_t n, int_inverse * res);
 uint32_t divide32_using_inverse(uint32_t n, int_inverse *inv);
 uint64_t divide64_using_inverse(uint64_t n, int_inverse *inv);
 
-#define rdmcycle(x)  {				       \
-    uint32_t lo, hi, hi2;			       \
-    __asm__ __volatile__ ("1:\n\t"		       \
-			  "csrr %0, mcycleh\n\t"       \
-			  "csrr %1, mcycle\n\t"	       \
-			  "csrr %2, mcycleh\n\t"       \
-			  "bne  %0, %2, 1b\n\t"			\
-			  : "=r" (hi), "=r" (lo), "=r" (hi2)) ;	\
-    *(x) = lo | ((uint64_t) hi << 32); 				\
-  }
+#define rdmcycle(x) {                                         \
+  uint32_t lo, hi, hi2;                                       \
+  __asm__ __volatile__ ("1:\n\t"                              \
+                        "csrr %0, mcycleh\n\t"                \
+                        "csrr %1, mcycle\n\t"                 \
+                        "csrr %2, mcycleh\n\t"                \
+                        "bne  %0, %2, 1b\n\t"                 \
+                        : "=r" (hi), "=r" (lo), "=r" (hi2)) ; \
+  *(x) = lo | ((uint64_t) hi << 32);                          \
+}
 
 /**
  * \brief Returns the number of milliseconds since the board began running the current program.
  *
  * \return Number of milliseconds since the program started (uint32_t)
  */
-extern uint32_t millis( void ) ;
+extern uint32_t millis(void);
 
 /**
  * \brief Returns the number of microseconds since the board began running the current program.
  *
  * \note There are 1,000 microseconds in a millisecond and 1,000,000 microseconds in a second.
  */
-extern uint32_t micros( void ) ;
+extern uint32_t micros(void);
 
 /**
  * \brief Pauses the program for the amount of time (in miliseconds) specified as parameter.
@@ -86,7 +83,7 @@ extern uint32_t micros( void ) ;
  *
  * \param dwMs the number of milliseconds to pause (uint32_t)
  */
-extern void delay( uint32_t dwMs ) ;
+extern void delay(uint32_t dwMs);
 
 /**
  * \brief Pauses the program for the amount of time (in microseconds) specified as parameter.
